ab3b25bea2b5a04998a1093d9fc9713a6a629c2a
Temuulen <temuulen.och@gmail.com>
2026-01-14 16:39:14 +0300

    s

--- oyunsbot.py
+++ oyunsbot.py
@@ -47,7 +47,7 @@ def is_within_ub_business_hours():
     return start <= now_ub <= end
 
 # Replace with your bot token
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip().replace('"', '').replace("'", "")
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://oyunswebapp.ddns.net")
 bot = telebot.TeleBot(BOT_TOKEN)
 
